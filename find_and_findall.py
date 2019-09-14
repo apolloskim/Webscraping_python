08d1288d2619fa6f6f5e1e5562f9f3225b885496
Apollos <apolloskim94@gmail.com>
2019-09-13 23:50:46 -0400

    Change the url

--- find_and_findall.py
+++ find_and_findall.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
 bs = BeautifulSoup(html.read(), 'html.parser')
 
 nameList = bs.findAll('span', {'class': 'green'})
